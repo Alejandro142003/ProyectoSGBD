9e1d959c3c4fcd6ba4646098e059d5f511622f64
Isabel <isabelroldancordoba@hotmail.com>
2022-12-13 12:41:38 +0100

    baseAlmacenBaseArticulos

--- src/Modelo/Almacen.java
+++ src/Modelo/Almacen.java
@@ -1,5 +1,59 @@
 package Modelo;
 
 public class Almacen {
+	public String nombre;
+	public String direccion;
+	public String telefono;
+	public int superficie = 0;
+	public Almacen() {
+		
+	}
+	
+	public Almacen (String nombre, String direccion, String telefono, int superficie) {
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.superficie = superficie;
+	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public int getSuperficie() {
+		return superficie;
+	}
+
+	public void setSuperficie(int superficie) {
+		this.superficie = superficie;
+	}
+
+	@Override
+	public String toString() {
+		return "Almacen [nombre=" + nombre + ", direccion=" + direccion + ", telefono=" + telefono + ", superficie="
+				+ superficie + "]";
+	}
+	
+	
+	
 }
