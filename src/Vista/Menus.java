d3c50dd582968e36b9ccd52b39245887779c3f65
martaalopez <119664444+martaalopez@users.noreply.github.com>
2022-12-13 21:13:29 +0100

    Configuracion

---
+++ src/Vista/Menus.java
@@ -0,0 +1,67 @@
+package Vista;
+import Utilidades.*;
+public class Menus{ 
+	 
+	 public static void muestraMenu() {
+		System.out.println("Menu");
+		System.out.println("Elige una opcion");
+		System.out.println("1.Introducir articulos");
+		System.out.println("2.Modificar articulo");
+		System.out.println("3.Borrar articulo");
+		System.out.println("4.Salir");
+	 }	 
+	
+ public static  int introduceOpcion() {
+	 int opcion=-1;
+	 boolean valido=false;
+	 do {
+		 valido=false;
+		 opcion=Introducir.getInt("Elige una opcion");
+		 if((opcion<1)||(opcion>5)){
+			System.out.println("Debe de introducir un opción valida");
+			valido=true;
+		 }
+	 }while(valido);
+	 return opcion;
+ }
+ public static void modificar() {
+	 System.out.println("Menu Modificar");
+	 System.out.println("Elige una opcion");
+	 System.out.println("1.Modifica el nombre");
+	 System.out.println("2.Modifica la cantidad");
+	 System.out.println("3.Modifica el precio");
+	 System.out.println("4.Modifica la descripcion");
+ }
+
+ public static int chooseOpcion() {
+	 int opcion=-1;
+	 boolean valido=false;
+	 do {
+		 valido=false;
+		 opcion=Introducir.getInt("Elige una opcion");
+		 if((opcion<1)||(opcion>4)){
+			System.out.println("Debe de introducir un opción valida");
+			valido=true;
+		 }
+	 }while(valido);
+	 return opcion;
+ }
+ public static void valModificar() {
+	 System.out.println("¿Está seguro de que quiere realizar los cambios efectuados?");
+	 System.out.println("1.Si");
+	 System.out.println("2.No");
+ }
+ public static int chooseOption() {
+	 int opcion=-1;
+	 boolean valido=false;
+	 do {
+		 valido=false;
+		 opcion=Introducir.getInt("Elige una opcion");
+		 if((opcion<1)||(opcion>2)) {
+			 System.out.println("Debe de introducir una opcion valida");
+			 valido=true;
+		 }
+	 }while(valido);
+	 return opcion;
+ }
+ }
